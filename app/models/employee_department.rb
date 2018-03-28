97e9027c317b5b6103231a678f2ae431159bb6e5
Katuyuki Kakigi <kakimomokuri@gmail.com>
2018-03-28 14:24:23 +0900

    既存DBを使ったアプリ作成

--- app/models/employee_department.rb
+++ app/models/employee_department.rb
@@ -2,4 +2,5 @@ class EmployeeDepartment < ApplicationRecord
   self.table_name='AT021'
   self.primary_keys=:NRJYUGYOIN, :DTSYOZOKUNENGETSU
   belongs_to :employee, :foreign_key => [:NRJYUGYOIN, :DTSYOZOKUNENGETSU]
+  belongs_to :department, :foreign_key => [:CDBUMON]
 end
